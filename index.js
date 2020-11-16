bc0322b8bfbbdf73c2fe15f43439df9432a1b32a
s-satsangi <s.satsangi@gmail.com>
2020-11-16 00:40:58 -0500

    Oh, documentation is a must!

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let main = document.getElementById('main');
+main.remove();
+let element = document.createElement('div');
+document.body.appendChild(element);
+let newHeader = document.createElement('h1');
+element.appendChild(newHeader);
+newHeader.id = 'victory';
+newHeader.innerHTML = "YOUR-NAME is the champion";
